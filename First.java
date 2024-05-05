41be141ff2543ec869c43ec423bf57604f8d0caa
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-05-05 20:19:08 +0530

    Create First.java
    basicloops

---
+++ First.java
@@ -0,0 +1,33 @@
+class First{
+
+  public static void main(String[] args){
+     
+    int i=0;//local variable inside a method or method parmeter 
+
+    do{//do while is  flow statement its exit control loop .this iterative statement 
+      //dowhile block is  start
+        System.out.println(i);
+         i++;
+      }while(i<=5)// do while end  its work on i=5
+
+     i=1;
+
+    while(i<=5)//its entry control loop
+      {
+          System.out.println(i);
+          i++;
+      }
+     for(i=1;i<=50;i++){//work of for initializaton ;condition ;icre/decre  1.)init 2.)check con 3.)execute body 4.)incre/decre
+        // check condtion ,execute body,incre/decrement next is based on condtion repertely theis statte 2to
+       
+         System.out.println(i);
+     }
+
+
+
+    
+      }
+      
+  }
+
+}
