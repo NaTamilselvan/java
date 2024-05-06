dac16a4482d9b620a87d9aae3cb023cb44b2346d
N.Tamilselvan <natamilselvanme@gmail.com>
2024-05-06 06:26:20 +0530

    This interface may 6 moday interface practise

---
+++ Salesman.java
@@ -0,0 +1,33 @@
+public class Salesman implements Salesmanrules{
+
+
+public static void main(String[] args){
+
+//salary=150000;
+
+ Salesman s=new Salesman();
+
+System.out.println(salary);
+
+Salesmanrules.diplay1();
+
+}
+
+
+
+public void keepTime(){
+
+}
+
+public void cleaningThings(){
+}
+
+public void teaBreak(){
+
+}
+
+public void display(){
+}
+
+
+}
---
+++ Salesmanrules.java
@@ -0,0 +1,35 @@
+public interface Salesmanrules{
+
+//inside interface by default abstract 
+
+//static and final variable
+
+//you cannot provide body of instance methods 
+
+//you can provide a static member to define a body interface 
+
+int salary=10000, leave=4;
+
+public void keepTime();
+
+public void cleaningThings();
+
+public void teaBreak();
+
+public void display();
+
+
+public static void diplay1(){
+
+System.out.println("i am static method");
+
+}
+
+
+
+
+
+
+
+
+}
