7340f067a925a87746bb3a2f5aa644f1dabcc1c5
N.Tamilselvan <natamilselvanme@gmail.com>
2024-06-05 21:02:27 +0530

    today practise

---
+++ Testu.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class Testu{
+
+public static void main(String[] args){
+ 
+  HashMap re=new HashMap();
+   
+    System.out.println(re);  
+
+   re.put("FirstName","Tamil");
+   re.put("FirstName","Selvan");   
+   re.put("First","fdd");
+  System.out.println(re);
+
+}
+
+
+
+}
