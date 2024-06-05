e6e846219079e6835474815f034dca9f06f7193b
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-06-04 23:36:11 -0600

    Create THreadDemo.java
    Thread program is  github

---
+++ THreadDemo.java
@@ -0,0 +1,20 @@
+// what is thread line of execution inour program has single thread the is squence or line by line excution .A program under execution of  RAM is called process/Task .
+//A java Automatticlaiiu called main stack an d software is threadscduker it create which thread is currently execute method is .   
+
+
+class THreadDemo{
+
+
+public static void main(String[] args){
+
+
+ Thread  t=Thread.currentThread(); 
+
+System.out.println(t);
+
+
+}
+
+
+
+}
