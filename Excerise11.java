ccb2acab0b9bb0b6065458fd6f82785ffb8aa890
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-06-11 03:43:20 -0600

    Create Excerise11.java
    This program practise windows machine

---
+++ Excerise11.java
@@ -0,0 +1,34 @@
+class Excerise11{
+
+public static void print(){
+
+
+int a,b;
+
+a=10;
+
+b=20;
+
+a=a+b;
+
+b=a-b;
+
+a=a-b;
+
+System.out.println("The value of  a"+a+"The value of  b"+b);
+
+
+}
+
+
+
+}
+
+public class Demo{
+
+
+
+public static void main(Strin
+
+
+}
