26ee62cef96fcfcc56617e67ea05a54e3aa43b1f
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-06-16 12:07:33 +0530

    Create ArrayCheck.java
    Array recall

---
+++ ArrayCheck.java
@@ -0,0 +1,17 @@
+package com.tamil.package1;
+
+public class ArrayCheck {
+
+	public static void main(String[] args) {
+		
+	
+		int[] s=new int[3];
+		
+		for(int i=-1;i<s.length;i++) {
+			System.out.println(s[i]);
+		}
+		
+
+	}
+
+}
