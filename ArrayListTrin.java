d74a6b36fc214deb563d986f550fd42e2b4bb430
N.Tamilselvan <natamilselvanme@gmail.com>
2024-07-02 20:25:06 +0530

    today working

---
+++ ArrayListTrin.java
@@ -0,0 +1,40 @@
+ 
+import java.util.ArrayList;
+
+class ArrayListTrin{
+
+
+public static void main(String[] args){
+
+
+ ArrayList al=new ArrayList(5);
+  
+    al.add(10);
+
+    al.add(20);
+
+    al.add(30);
+
+    al.add(40);
+
+     al.add(50);
+
+   al.add(60);
+
+  System.out.println(al);
+
+ArrayList al2=new ArrayList(al);
+
+System.out.println(al2);
+
+
+
+
+}
+
+
+}
+
+
+
+
