bf74cb0fb96d15cffadd2dfbe6563f102c50efd2
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-07-17 01:43:42 -0600

    Door.java

---
+++ Door.java
@@ -0,0 +1,17 @@
+
+
+public class Door {
+	
+	int height , width ;
+	
+	
+	void DoorOpen() {
+		System.out.println("The door is opening ");
+	}
+	
+	void doorClose() {
+		
+	System.out.println("The door is close");
+	}
+
+}
