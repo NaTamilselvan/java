7d68c2a1367a393c7e7aa2e9732840e11b6b0fe5
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-23 14:52:26 +0530

    printAscii.py

---
+++ printAscii.py
@@ -0,0 +1,22 @@
+package EverydayRecall;
+
+public class PrintAscii {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		char ch='a';
+		
+		int a=ch;
+		
+	    System.out.println(a);
+		
+		
+		
+		
+
+	}
+
+}
