20752b9b73d1c700ea64bfd8d54ea0dadbdee245
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-24 14:20:59 +0530

    A pattern.java

---
+++ A pattern .java
@@ -0,0 +1,41 @@
+for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<=9;j++) {
+				
+				if(j-i==5  || i+j==5 || i==3 && j==5 )
+				System.out.print("*");
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+		
+		
+		
+}
+
+
+
+
+
+
+
+
+for(int i=1;i<5;i++) {
+	
+	for(int j=1;j<=5;j++) {
+		
+		if(i==1 ||i==3 || j==1 ||j==5 )
+		System.out.print("*");
+		else {
+			System.out.print(" ");
+		}
+	}
+	
+	System.out.println();
+
+
+
+}
+
