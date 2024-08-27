7a47a32cbe8dd9567acee8c68c47dea8b00a827a
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-27 02:24:19 -0600

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        
+        String s="hellow world";
+       
+       System.out.print(s.substring(1,s.length() -1));
+       
+       System.out.println();
+        
+        for(int i=1;i<s.length()-1;i++){
+            
+           System.out.print(s.charAt(i));
+        
+            
+        }
