466831fa2224b2633c99d1d0cfad47661fbfbc06
N.Tamilselvan <natamilselvanme@gmail.com>
2024-12-14 21:30:04 +0530

    today practished programs ArrayList

---
+++ Primitive.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+class Datatypes{
+
+//class is start
+
+
+public static void main(String[] args){
+
+
+//variable is data container its store values and text
+
+//every variable to assocaiated with data type for prober memory allocation ,based on operations
+
+//variable type variable name;
+
+ArrayList al=new ArrayList();
+
+al.add(10);
+
+al.add(20);
+
+al.add(30);
+
+al.add(40);
+
+
+System.out.println(al);
+
+}
+
+
+
+//class is end 
+
+}
