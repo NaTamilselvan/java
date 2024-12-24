842531725757849b6a997b5c226172b51439fc77
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-12-24 07:10:26 -0600

    Testmain.java
    Today practice program

---
+++ Testmain.java
@@ -0,0 +1,42 @@
+package Super;
+
+class Test{
+	//This program for practice for Array 
+	/* Array is store more then one value same data type and fixed size( homgenous data type )
+*/
+	String name;
+	int mark1,mark2,mark3,mark4,mark5;
+
+	}
+
+	public class Testmain{
+
+	public static void main(String [] args){
+
+	Test[] t;
+
+	t=new Test[3];
+	
+	t[0].name="TamilSelvan";
+	
+	t[0].mark1=10;
+	
+	t[0].mark1=10;
+	
+	t[0].mark1=10;
+
+	t[0].mark1=10;
+	
+	t[0].mark1=10;
+
+	System.out.println(t[0].name+t[0].mark1+t[0].mark2);
+
+
+
+
+
+	}
+
+
+
+	}
