b4105fd976183a1a41c28798d97ff7e0999c0501
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-12-25 10:28:00 -0600

    Arraytrial.java
    Today practiced program

---
+++ Arraytrial.java
@@ -0,0 +1,25 @@
+class Fruits{
+
+String name;
+Fruits(String name){
+
+this.name=name;
+
+}
+
+
+}
+
+class Arraytrial{
+
+public static void main(String[] args){
+
+
+ Arraytrial ar=new Arraytrial[3];
+
+ar[0]=new Fruits("mango");
+
+}
+
+
+}
