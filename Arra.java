fed06eba8611eda9d85741b19550687f606e644c
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-12-28 09:56:11 -0600

    Arra.java
    Today practiced program

---
+++ Arra.java
@@ -0,0 +1,17 @@
+class Arra{
+
+public static void main(String[] args){
+
+
+//Arrays is present in java.lang package 
+//every class in java are non primitive type and all class parent class is Object
+
+String[] s={"Tamilselvan","Tamilselvi","B.tech"};
+
+Arrays.sort(s);
+
+
+}
+
+
+}
