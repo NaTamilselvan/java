b4f047c27b6b0b27d14661d2fda1f0e24beaf713
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2025-02-08 13:22:09 +0530

    Add files via upload
    These programs are i was recall the java program  about for import and package statement

---
+++ Myclass.java
@@ -0,0 +1,15 @@
+package com.oops.myclass;
+
+public class Myclass{
+
+
+public Myclass(){
+
+System.out.println("WElcome my class");
+}
+
+  public String name="my class";
+
+
+
+}
---
+++ Otherclass.java
@@ -0,0 +1,19 @@
+package com.oops.otherclass;
+
+import com.oops.myclass.Myclass;
+
+public class Otherclass{
+
+public static void main(String[] args){
+
+System.out.println("hello");
+
+Myclass o=new Myclass(); 
+
+System.out.println(o.name);
+
+}
+
+
+
+}
