927582661926268f81a3c434514ab0b388d48799
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2025-04-10 15:28:49 +0530

    Two_dimensional_Array.java

---
+++ Two_dimensional_Array.java
@@ -0,0 +1,29 @@
+public class Two_dimensional_Array {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int[][] arr=new int[3][];
+		
+		arr[0]= new int[3];
+		
+		arr[1]=new int[2];
+
+		arr[2]=new int[5];
+		
+		System.out.println(arr.length);
+		
+		System.out.println(arr[0].length);
+		
+		System.out.println(arr[1].length);
+		
+		System.out.println(arr[2].length);
+
+		
+		
+		
+		
+	}
+
+}
