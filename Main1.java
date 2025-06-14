054cce4100a22b1efd16eb19485960f889f51a2f
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2025-06-14 14:17:06 +0530

    Main1.java
    This program try parseInt

---
+++ Main1.java
@@ -0,0 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main1 {
+    public static void main(String[] args) {
+        
+        String s="123.2";
+       float num1 = Float.parseFloat(s);
+       
+       
+       System.out.println(num1);
+    }
+}
